db1ad7eb28a94b6fd8c3c68ade94a7d7bea57b18
Anushka <899anushkasingh@gmail.com>
2025-09-30 23:56:53 +0530

    Dashboard in making

--- Dashboard/src/components/shared/Sidebar.jsx
+++ Dashboard/src/components/shared/Sidebar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { FcBullish } from "react-icons/fc";
+import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/Navigation';
+import {Link} from 'react-router-dom';
+
+
+const linkClasses = ' text-gray-300 flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
 
 
@@ -10,10 +15,25 @@ const Sidebar = () => {
                 <FcBullish size={25} /> 
                 <span className='text-neutral-300 text-lg'>Open Shop</span>
             </div>
-            <div className='flex-1'>top part</div>
-            <div className=''>bottom part</div>
+            <div className='flex-1 py-8 flex-col gap-0.5'>
+                {DASHBOARD_SIDEBAR_LINKS.map((item)=>(
+                    <SidebarLink key={item.key} item={item}>
+                    </SidebarLink>
+                        
+                ))}
+            </div>
+            <div>bottom part</div>
         </div>
     )
 }
 
 export default Sidebar
+
+function SidebarLink({item}){
+    return (
+        <Link to={item.path} className={linkClasses}>
+        <span className='text-xl'>{item.icon}</span>
+        {item.label}
+        </Link>
+    )
+}
